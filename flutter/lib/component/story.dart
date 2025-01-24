ff261f3e8e1bb715533fc858c102e4f99c756b96
chanxxw <strauss2327@gmail.com>
2025-01-24 18:20:49 +0900

    update

--- flutter/lib/component/story.dart
+++ flutter/lib/component/story.dart
@@ -9,22 +9,22 @@ class Story {
 final List<Story> stories = [
   Story(
     title: '반짝이의 이빨 모험',
-    imagePath: 'assets/무료 동화/반짝이의 이빨 모험/images/title.png',
+    imagePath: 'assets/freeStory/freeStory001/images/title.png',
     storyId: 'freeStory001',
   ),
   Story(
     title: '해와 달이 된 오누이',
-    imagePath: 'assets/무료 동화/해와 달이 된 오누이/images/title.png',
+    imagePath: 'assets/freeStory/freeStory002/images/title.png',
     storyId: 'freeStory002',
   ),
   Story(
     title: '의좋은 형제',
-    imagePath: 'assets/무료 동화/의좋은 형제/images/title.png',
+    imagePath: 'assets/freeStory/freeStory003/images/title.png',
     storyId: 'freeStory003',
   ),
-  // Story(
-  //   title: '금도끼 은도끼',
-  //   imagePath: 'assets/무료 동화/금도끼 은도끼/images/title.png',
-  //   storyId: 'freeStory004',
-  //),
+  Story(
+    title: '금도끼 은도끼',
+    imagePath: 'assets/freeStory/freeStory004/images/title.png',
+    storyId: 'freeStory004',
+  ),
 ];
